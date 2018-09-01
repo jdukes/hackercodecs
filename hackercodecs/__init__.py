58b6f8811dafdc8f52bde8c0605ec6017667f366
Josh Dukes <hex@neg9.org>
2018-09-01 09:37:22 -0700

    fixed incorrect indentation

--- hackercodecs/__init__.py
+++ hackercodecs/__init__.py
@@ -274,7 +274,7 @@ yenc_escape = [0x00, 0x0a, 0x0d, ord('='), ord('.')]
 
 def blocks(data, size):
     assert (len(data) % size) == 0, \
-           "Cannot divide into blocks of size %s" % size
+        "Cannot divide into blocks of size %s" % size
     for i in xrange(0, len(data), size):
         yield data[i:i + size]
 
