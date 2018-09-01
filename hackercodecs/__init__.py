06c1042bcaa3f75e282cb5b1c52a523d08df57b8
Josh Dukes <hex@neg9.org>
2018-09-01 10:24:36 -0700

    fixed u'\xaa' rot to v because unicode wat

--- hackercodecs/__init__.py
+++ hackercodecs/__init__.py
@@ -287,9 +287,16 @@ def parity(bit_array, odd=False):
 
 
 def rotx(data, rotval):
+    if len(data) == 0:
+        return data
     output = []
     for d in data:
-        if not d.isalpha():
+        if (not d.isalpha()) or (
+                # cause
+                # u'\xaa'.isalpha() == True
+                # ^ wat
+                ord(d) < ord('A') or
+                ord(d) > ord('z')):
             output.append(d)
             continue
         off = 65
